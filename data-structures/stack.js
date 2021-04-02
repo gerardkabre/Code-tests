b1d36b52e8a0b04774a0455a18e8a892b5265a36
gerardkabre <gerard.kabre@gmail.com>
2021-04-02 12:43:05 +0200

    remove comment

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -31,12 +31,6 @@ myStack.push(value)
 Create a contains method to check if a value is in the stack:
 myStack.contains('findme')
 => true/false
-What's the time complexity?
-Create an until method to get the number of pops until you get to a certain value:
-stack values - (first)2-5-7-3-6-9(last)
-myStack.until(7)
-=> 4
-What's the time complexity?
  */
 
 export function Stack(capacity) {
@@ -90,19 +84,3 @@ Stack.prototype.contains = function (valueToCheck) {
     (value) => value === valueToCheck
   );
 };
-
-/*
-*** Exercises:
-1. Implement a stack with a min method which returns the minimum element currently in the stack. 
-This method should have O(1) time complexity. Make sure your implementation handles duplicates.
-2. Sort a stack so that its elements are in ascending order.
-3. Given a string, determine if the parenthesis in the string are balanced.
-Ex: balancedParens( 'sqrt(5*(3+8)/(4-2))' ) => true
-Ex: balancedParens( 'Math.min(5,(6-3))(' ) => false
-4. Towers of Hanoi - https://en.wikipedia.org/wiki/Tower_of_Hanoi
-You are given three towers (stacks) and N disks, each of different size. You can move the disks according to three constraints:
-   1. only one disk can be moved at a time
-   2. when moving a disk, you can only use pop (remove the top element) and push (add to the top of a stack)
-   3. no disk can be placed on top of a disk that is smaller than it
-The disks begin on tower#1. Write a function that will move the disks from tower#1 to tower#3 in such a way that none of the constraints are violated.
- */
